fff134dab9a6e772f7273144e579719f0909bd94
Duy Le <asianinvasion.le@gmail.com>
2023-01-15 12:11:30 -0500

    replaced all pokemon image sources

--- index.js
+++ index.js
@@ -34,23 +34,23 @@ app.get('/api/pokemon/:id', (req, res) => {
 })
 
 app.get('/api/gen/:id', (req, res) => {
-	if (req.params.id === '1') {
-		return res.status(200).send(data.slice(0, 151))
-	} else if (req.params.id === '2') {
-		return res.status(200).send(data.slice(151, 251))
-	} else if (req.params.id === '3') {
-		return res.status(200).send(data.slice(251, 386))
-	} else if (req.params.id === '4') {
-		return res.status(200).send(data.slice(386, 493))
-	} else if (req.params.id === '5') {
-		return res.status(200).send(data.slice(493, 649))
-	} else if (req.params.id === '6') {
-		return res.status(200).send(data.slice(649, 721))
-	} else if (req.params.id === '7') {
-		return res.status(200).send(data.slice(721, 809))
-	} else if (req.params.id === '8') {
-		return res.status(200).send(data.slice(809, 905))
-	}
+	if (req.params.id === '1') return res.status(200).send(data.slice(0, 151))
+
+	if (req.params.id === '2') return res.status(200).send(data.slice(151, 251))
+
+	if (req.params.id === '3') return res.status(200).send(data.slice(251, 386))
+
+	if (req.params.id === '4') return res.status(200).send(data.slice(386, 493))
+
+	if (req.params.id === '5') return res.status(200).send(data.slice(493, 649))
+
+	if (req.params.id === '6') return res.status(200).send(data.slice(649, 721))
+
+	if (req.params.id === '7') return res.status(200).send(data.slice(721, 809))
+
+	if (req.params.id === '8') return res.status(200).send(data.slice(809, 905))
+
+	if (req.params.id === '9') return res.status(200).send(data.slice(905, 1008))
 
 	res.status(404).send('Generation not found')
 })
